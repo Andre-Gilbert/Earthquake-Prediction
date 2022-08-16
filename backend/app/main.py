0bbf69622e1d9023f4d0c923552f5d4470f5535c
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-08-16 13:01:06 +0200

    added folders and updated pylintrc

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,15 +8,8 @@ app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1_STR}/o
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*'],
-    )
-
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+    app.add_middleware(CORSMiddleware,
+                       allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+                       allow_credentials=True,
+                       allow_methods=['*'],
+                       allow_headers=['*'])
