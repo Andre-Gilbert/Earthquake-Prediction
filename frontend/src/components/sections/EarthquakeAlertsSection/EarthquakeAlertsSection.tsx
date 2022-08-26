304b3e727d0f414d68dc25c9277e0cb172ba2881
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-08-26 16:54:19 +0200

    added content to alerts

--- frontend/src/components/sections/EarthquakeAlertsSection/EarthquakeAlertsSection.tsx
+++ frontend/src/components/sections/EarthquakeAlertsSection/EarthquakeAlertsSection.tsx
@@ -1,8 +1,10 @@
-import { Button, Card, H5, Icon, Intent } from '@blueprintjs/core';
+import { Button, Card, Checkbox, Classes, H5, Icon, Intent, Menu, MenuDivider } from '@blueprintjs/core';
+import { Popover2, Tooltip2 } from '@blueprintjs/popover2';
 import { UseQueryResult } from '@tanstack/react-query';
 import { useEarthquakeAlerts } from 'queries/earthquakes';
 import { useMemo, useState } from 'react';
 import styles from './EarthquakeAlerts.module.scss';
+import { getIntent, getMagnitudeTypeTooltip } from './utils';
 
 export const EarthquakeAlertsSection = () => {
     const [alertLevels, setAlertLevels] = useState(['green', 'yellow', 'orange', 'red']);
@@ -25,17 +27,28 @@ export const EarthquakeAlertsSection = () => {
 };
 
 const Header = () => {
-    const renderInputValue = (data: any) => data.alert;
-
     return (
         <div className={styles.header}>
             <div className={styles.alertsTitle}>
                 <Icon icon="warning-sign" />
                 <H5>Earthquake Alerts</H5>
             </div>
-            {/* <Suggest2 inputValueRenderer={renderInputValue} /> */}
-            <Button icon="filter" minimal />
+            <Popover2 content={<FilterMenu />} placement="bottom-end">
+                <Button icon="filter" minimal />
+            </Popover2>
         </div>
+    );
+};
+
+const FilterMenu = () => {
+    return (
+        <Menu>
+            <MenuDivider title="Alert Levels" />
+            <Checkbox>Green</Checkbox>
+            <Checkbox>Yellow</Checkbox>
+            <Checkbox>Orange</Checkbox>
+            <Checkbox>Red</Checkbox>
+        </Menu>
     );
 };
 
@@ -43,25 +56,16 @@ type AlertsProps = {
     alerts: UseQueryResult<any, unknown>[];
 };
 
-const getIntent = (alertLevel: string) => {
-    switch (alertLevel) {
-        case 'green':
-            return Intent.SUCCESS;
-        case 'yellow':
-            return Intent.PRIMARY;
-        case 'orange':
-            return Intent.WARNING;
-        case 'red':
-            return Intent.DANGER;
-        default:
-            return Intent.NONE;
-    }
-};
-
 const Alerts = ({ alerts }: AlertsProps) => {
     return (
         <>
-            <div className={styles.alert}>Labels</div>
+            <div className={styles.alertLabels}>
+                <p className={Classes.TEXT_MUTED}>Date</p>
+                <p className={Classes.TEXT_MUTED}>Place</p>
+                <p className={Classes.TEXT_MUTED}>Magnitude</p>
+                <p className={Classes.TEXT_MUTED}>Magnitude Type</p>
+                <p className={Classes.TEXT_MUTED}>Depth</p>
+            </div>
             {alerts.map(alert =>
                 alert.data.features.map(
                     (earthquake: any) =>
@@ -71,6 +75,9 @@ const Alerts = ({ alerts }: AlertsProps) => {
                                 time={earthquake.properties.time}
                                 place={earthquake.properties.place}
                                 magnitude={earthquake.properties.mag}
+                                magnitudeType={earthquake.properties.magType}
+                                magnitudeTypeTooltip={getMagnitudeTypeTooltip(earthquake.properties.magType)}
+                                depth={earthquake.geometry.coordinates[2]}
                                 intent={getIntent(earthquake.properties.alert)}
                             />
                         ),
@@ -80,17 +87,43 @@ const Alerts = ({ alerts }: AlertsProps) => {
     );
 };
 
-type AlertProps = { time: number; place: string; magnitude: string; intent: Intent };
+type AlertProps = {
+    time: number;
+    place: string;
+    magnitude: string;
+    magnitudeType: string;
+    magnitudeTypeTooltip: { magnitudeRange: string; distanceRange: string };
+    depth: number;
+    intent: Intent;
+};
 
-const Alert = ({ time, place, magnitude, intent }: AlertProps) => {
+const Alert = ({ time, place, magnitude, magnitudeType, magnitudeTypeTooltip, depth, intent }: AlertProps) => {
     const date = new Date(time);
 
     return (
         <div className={styles.alert}>
             <Icon icon="info-sign" intent={intent} />
-            <div className={styles.alertDate}>{date.toLocaleString()}</div>
-            <div className={styles.alertPlace}>{place}</div>
-            <div className={styles.alertMagnitude}>Magnitude: {magnitude}</div>
+            <div className={styles.alertContent}>{date.toLocaleString()}</div>
+            <div className={styles.alertContent}>{place}</div>
+            <div className={styles.alertContent}>{magnitude}</div>
+            <Tooltip2 className={styles.alertContent} content={<TooltipContent {...magnitudeTypeTooltip} />}>
+                {magnitudeType}
+            </Tooltip2>
+            <div className={styles.alertContent}>{depth}</div>
         </div>
+    );
+};
+
+type TooltipContentProps = {
+    magnitudeRange: string;
+    distanceRange: string;
+};
+
+const TooltipContent = ({ magnitudeRange, distanceRange }: TooltipContentProps) => {
+    return (
+        <>
+            <p className={styles.tooltip}>Magnitude range: {magnitudeRange}</p>
+            <p className={styles.tooltip}>Distance range: {distanceRange}</p>
+        </>
     );
 };
---
+++ frontend/src/components/sections/EarthquakeAlertsSection/utils.ts
@@ -0,0 +1,53 @@
+import { Intent } from '@blueprintjs/core';
+
+export const getIntent = (alertLevel: string) => {
+    switch (alertLevel) {
+        case 'green':
+            return Intent.SUCCESS;
+        case 'yellow':
+            return Intent.PRIMARY;
+        case 'orange':
+            return Intent.WARNING;
+        case 'red':
+            return Intent.DANGER;
+        default:
+            return Intent.NONE;
+    }
+};
+
+export const getMagnitudeTypeTooltip = (magnitudeType: string) => {
+    switch (magnitudeType.toLowerCase()) {
+        case 'mww':
+            return { magnitudeRange: '~5.0 and larger', distanceRange: '1 - 90  degrees' };
+        case 'mwc':
+            return { magnitudeRange: '~5.5 and larger', distanceRange: '20 - 180  degrees' };
+        case 'mwb':
+            return { magnitudeRange: '~5.5 to ~7.0', distanceRange: '30 - 90  degrees' };
+        case 'mwr':
+            return { magnitudeRange: '~4.0 to ~6.5', distanceRange: '0 - 10  degrees' };
+        case 'ms20' || 'ms':
+            return { magnitudeRange: '~5.0 to ~8.5', distanceRange: '20 - 160  degrees' };
+        case 'mb':
+            return { magnitudeRange: '~4.0 to ~6.5', distanceRange: '15 - 100 degrees' };
+        case 'mfa':
+            return { magnitudeRange: 'any', distanceRange: 'any' };
+        case 'ml':
+            return { magnitudeRange: '~2.0 to ~6.5', distanceRange: '0 - 600 km' };
+        case 'mb_lg' || 'mlg':
+            return { magnitudeRange: '~3.5 to ~7.0', distanceRange: '150 – 1110 km (10 degrees)' };
+        case 'md' || 'md':
+            return { magnitudeRange: '~4 or smaller', distanceRange: '0 - 400 km' };
+        case 'mi' || 'mwp':
+            return { magnitudeRange: '~5.0 to ~8.0', distanceRange: 'all' };
+        case 'me':
+            return { magnitudeRange: '~3.5 and larger', distanceRange: 'all' };
+        case 'mh':
+            return { magnitudeRange: 'any', distanceRange: 'any' };
+        case 'finite fault':
+            return { magnitudeRange: '~7.0 and larger', distanceRange: '30 - 90 degrees' };
+        case 'mint':
+            return { magnitudeRange: 'any', distanceRange: 'any' };
+        default:
+            return { magnitudeRange: 'any', distanceRange: 'any' };
+    }
+};
