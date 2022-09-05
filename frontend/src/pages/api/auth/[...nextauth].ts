56e5ff43278df8ec53f5dadfb319f0f29b906f2e
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-09-05 14:33:55 +0200

    added next auth env types

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ import GoogleProvider from 'next-auth/providers/google';
 const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID ?? '',
-            clientSecret: process.env.GOOGLE_SECRET ?? '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
 };
