90c957062095ca2e456998b96173e9db8371826a
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-10-27 16:57:03 +0700

    refactored server code

--- app/server/app/ml/utils.py
+++ app/server/app/ml/utils.py
@@ -3,10 +3,8 @@ from typing import Any
 from urllib import parse
 
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 
 _PARAMS = {'format': 'csv', 'eventtype': 'earthquake'}
-_SCALER = MinMaxScaler()
 
 
 def get_earthquakes_data(base_url: str, date_range: dict[str, str]) -> pd.DataFrame:
@@ -21,8 +19,3 @@ def build_url(base_url: str, params: dict[str, str]) -> str:
 
 def parse_df(df: pd.DataFrame) -> dict[str, Any]:
     return df.to_dict(orient='records')
-
-
-def normalize_df(df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
-    df[columns] = _SCALER.fit_transform(df[columns])
-    return df
