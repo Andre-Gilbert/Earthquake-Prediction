ebea864bfc1e98eb266dd4006863583170e814b5
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-11-01 16:57:18 +0700

    refactored code

--- app/client/src/config/axios.ts
+++ app/client/src/config/axios.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const USGS_EARTHQUAKE_API_URL = 'https://earthquake.usgs.gov/fdsnws/event/1';
 const EARTHQUAKE_API_URL = process.env.EARTHQUAKE_API_URL ?? 'http://localhost:8000';
-const EARTHQUAKE_API_KEY = '';
 
 export const usgsInstance = axios.create({
     baseURL: USGS_EARTHQUAKE_API_URL,
