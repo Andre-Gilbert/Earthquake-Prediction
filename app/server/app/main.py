d108586c4ae259b24b9f216c34f247453d834eb4
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-11-04 09:45:29 +0700

    test cors

--- app/server/app/main.py
+++ app/server/app/main.py
@@ -1,15 +1,14 @@
 """REST API."""
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.api import api
 from app.core.config import settings
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1_STR}/openapi.json')
 
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(CORSMiddleware,
-                       allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+                       allow_origins=['*'],
                        allow_credentials=True,
                        allow_methods=['*'],
                        allow_headers=['*'])
