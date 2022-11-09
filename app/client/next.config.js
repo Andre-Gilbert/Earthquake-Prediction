6df6f6eaa60fb53713d4f2678b4f9187fc754ed6
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-11-09 20:46:43 +0700

    updated config

--- app/client/next.config.js
+++ app/client/next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/v1/earthquakes/predict-magnitudes',
+                destination: 'https://earthquake-prediction.onrender.com/api/v1/earthquakes/predict-magnitudes',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
